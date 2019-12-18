a146155dfe576780f6f170e58764870cc11f49fd
et4321 <54795466+et4321@users.noreply.github.com>
2019-12-18 10:28:23 +0100

    Uitwerking van de exercise

--- wisselgeld.php
+++ wisselgeld.php
@@ -2,9 +2,8 @@
 
 
 $geld = ($argv[1]);
-$geld = round(($geld*100)/5)*5/100;
-define("GELD" , array(50, 20, 10, 5, 2, 1, 0.50, 0.20, 0.10, 0.05, 0.02, 0.01 ));
-
+$geld = round (($geld*100)/5)*5/100;
+define ("GELD" , array (50, 20, 10, 5, 2, 1, 0.50, 0.20, 0.10, 0.05, 0.02, 0.01 ));
 
  foreach (GELD as $bedrag) {
     $floored = 0;
@@ -15,18 +14,15 @@ define("GELD" , array(50, 20, 10, 5, 2, 1, 0.50, 0.20, 0.10, 0.05, 0.02, 0.01 ))
         $geld = round ($geld - ($floored *  $bedrag), 2);
     }
 
-
     if ($floored > 0){
-        if($bedrag < 1){
+        if ($bedrag < 1){
            $cent  = $bedrag * 100;
             echo "$floored * $cent cent".PHP_EOL;
-        }else{
+        }
+        else{
             echo"$floored * $bedrag euro".PHP_EOL;
         }
-
     }
 }
-
 exit;
-
 ?>
